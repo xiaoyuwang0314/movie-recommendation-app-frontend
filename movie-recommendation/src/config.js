aee6e372d35c1e57b4472d6e9e6b18d06cd0c2ac
xiaoyuwang0314 <wang.xiaoyu7@northeastern.edu>
2025-04-21 19:51:24 -0700

    base-url: https

--- movie-recommendation/src/config.js
+++ movie-recommendation/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || 'http://api.justanotherapp.me';
+    import.meta.env.VITE_API_BASE_URL;
 
 export const API_ENDPOINTS = {
     BASE_URL: API_BASE_URL,
